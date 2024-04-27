634b9804f42888fd59cda17eb8bc3642a7cb6f9b
Nathan Brown <nathan@nkbrown.us>
2024-04-27 09:38:19 -0700

    Use separate server and port for HMR

--- server.js
+++ server.js
@@ -8,14 +8,21 @@ import http from "node:http";
 installGlobals();
 
 const app = express();
-const server = http.createServer(app);
+
+const hmrApp = express();
+const hmrServer = http.createServer(hmrApp);
+hmrApp.use((_req, res, next) => {
+  res.setHeader("cross-origin-resource-policy", "cross-origin");
+  res.setHeader("access-control-allow-origin", "http://localhost:3000");
+  next();
+});
 
 const viteDevServer =
   process.env.NODE_ENV === "production"
     ? undefined
     : await import("vite").then((vite) =>
         vite.createServer({
-          server: { middlewareMode: true, hmr: { server } },
+          server: { middlewareMode: true, hmr: { server: hmrServer } },
         })
       );
 
@@ -50,7 +57,9 @@ app.use(morgan("tiny"));
 // handle SSR requests
 app.all("*", remixHandler);
 
+hmrServer.listen(8002);
+
 const port = process.env.PORT || 3000;
-server.listen(port, () =>
+app.listen(port, () =>
   console.log(`Express server listening at http://localhost:${port}`)
 );
--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
       //headers: {
       //  "cross-origin-resource-policy": "cross-origin",
       //}
+      port: 8002,
     },
   },
 });
